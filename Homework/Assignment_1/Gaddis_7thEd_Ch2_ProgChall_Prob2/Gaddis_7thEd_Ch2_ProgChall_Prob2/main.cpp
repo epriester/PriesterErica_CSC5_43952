f0825e2d54d3258dbbed8eb9cda8c7d3b24891e4
Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>
2015-03-09 04:10:20 -0700

    Signed-off-by: Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>

---
+++ Homework/Assignment_1/Gaddis_7thEd_Ch2_ProgChall_Prob2/Gaddis_7thEd_Ch2_ProgChall_Prob2/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ *Created on March 5, 2015
+ * Purpose: Homework Assignment 1
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    float totalSales=4600000; 
+    float salesPercent=0.62;
+    float total;
+    //Calculate the total
+    total=totalSales*salesPercent;
+    cout<<"If the East Coast division makes $"<<totalSales<<
+    cout<<" this year in sales."<<endl;
+    cout<<"The division will generate $" <<total<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
+
+
+
