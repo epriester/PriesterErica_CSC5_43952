c863c7da5d33087619b392743897db0440421a2f
Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>
2015-03-09 03:44:49 -0700

    Signed-off-by: Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>

---
+++ Homework/Assignment_1/Gaddis_7thEd_Ch2_ProgChall_Prob1/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ *Created on March 5, 2015, 5:34 PM
+ * Purpose: Homework Assignment 1
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+//Global constants
+
+//Function Prototypes 
+
+//Execution Begins Here!
+int main (int argc, char** argv) {
+    //Declare variables
+    int var1=62, var2=99, total;
+    //Calculate the total
+    total= var1 + var2;
+    cout<<"total:"<<total;
+    //Exit Stage Right
+    return 0;
+}
+
