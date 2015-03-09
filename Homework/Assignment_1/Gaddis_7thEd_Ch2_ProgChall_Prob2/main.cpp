ab2acb719af63fc5a458ad75bc6205c0d8f9a23a
Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>
2015-03-09 03:15:54 -0700

    Signed-off-by: Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>

---
+++ Homework/Assignment_1/Gaddis_7thEd_Ch2_ProgChall_Prob2/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ *
+ * Created on March 8, 2015, 6:27 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+
+    //Exit stage right!
+    return 0;
+}
+
