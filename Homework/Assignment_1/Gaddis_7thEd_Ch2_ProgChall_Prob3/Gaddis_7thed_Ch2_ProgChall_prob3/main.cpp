befc9c854ed444b7deb5caf5d8260b8987bc070e
Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>
2015-03-09 17:03:38 -0700

    Signed-off-by: Ericagit config --global user.email epriester01@yahoo.com <epriester01@Ericas-MacBook-Pro.local>

---
+++ Homework/Assignment_1/Gaddis_7thEd_Ch2_ProgChall_Prob3/Gaddis_7thed_Ch2_ProgChall_prob3/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ * Created on March 9, 2015
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Declare the variables 
+    float purchase= 52, stateTax= 0.04, cntryTax= 0.02, totalTax; 
+    //Calculate the total
+    totalTax= purchase*(stateTax+cntryTax);
+    cout<<"If you make a $"<<purchase<<" purchase, "<<endl;
+    cout<<"With a state sales tax of 4% and a country sales tax of 2%"<<endl;
+    cout<<"The total sales tax will be $"<<totalTax<<endl;
+    //Exit stage right!
+    
+    return 0;
+}
+
