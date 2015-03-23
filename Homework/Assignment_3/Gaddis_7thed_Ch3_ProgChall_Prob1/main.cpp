a9605f239c20b44408dda39a2194939425d5c336
epriester <epriester01@yahoo.com>
2015-03-23 10:54:14 -0700

    Signed-off-by: epriester <epriester01@yahoo.com>

---
+++ Homework/Assignment_3/Gaddis_7thed_Ch3_ProgChall_Prob1/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ * Created on March 23, 2015, 10:23 AM
+ * Purpose: Find the minimum and maximum between two numbers
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+ //Declare Variables
+    float num1, num2;
+    
+    //Prompt user for input
+    cout<<"What is the first number?"<<endl;
+    cin>>num1;
+    cout<<"What is the second number?"<<endl;
+    cin>>num2;
+    //Calculate Conditional Operator
+    if (num1>num2);
+    else (num1<num2);   
+    cout<<"The first number "<<num1<<" is larger, and the second number "<<
+    cout<<num2<<" is smaller"<<endl;       
+    cout<<"The first number "<<num1<<" is smaller than the second number "<<
+    cout<<num2<<endl;
+    
+ //Exit Stage Right!       
+    return 0;
+}
+
