8468dcc740cfdc01698ec1f48f548c56265d2958
epriester <epriester01@yahoo.com>
2015-04-06 10:53:08 -0700

    Signed-off-by: epriester <epriester01@yahoo.com>

---
+++ Homework/Assignment_4/Gaddis_7thed_Ch5_ProgChall_Prob6/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Erica Priester
+ * Created on April 6, 2015, 10:20 AM
+ * Purpose: Display a table of the ocean level over the next 25 years
+ */
+
+
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) 
+{
+//Declare Variables
+    float sume=0;
+    cout<<""
+    
+    return 0;
+}
+
